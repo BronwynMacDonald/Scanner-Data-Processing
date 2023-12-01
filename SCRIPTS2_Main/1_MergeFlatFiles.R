80b18d55e0c434143b18cd88698132cd918046e4
SOLV-Code <gpestal@solv.ca>
2023-12-01 01:49:29 -0800

    metric calcs run - basic version

--- SCRIPTS2_Main/1_MergeFlatFiles.R
+++ SCRIPTS2_Main/1_MergeFlatFiles.R
@@ -49,9 +49,9 @@ flat.ok.sk.cu <- read.csv("DATA_PROCESSING/Cleaned_FlatFile_ByCU_OkanaganSockeye
                        names(flat.fr.pk.cu)),names(flat.fr.cm.cu)),names(flat.ok.sk.cu))
 
 #                                 names(flat.isc.cm.cu)),names(flat.ok.sk.cu))
-vars.use <-intersect(intersect(intersect(
-                     intersect(names(flat.fr.sk.cu),names(flat.sbc.ck.cu)),names(flat.fr.co.cu)),
-                     names(flat.fr.pk.cu)),names(flat.fr.cm.cu))
+#vars.use <-intersect(intersect(intersect(
+#                     intersect(names(flat.fr.sk.cu),names(flat.sbc.ck.cu)),names(flat.fr.co.cu)),
+#                     names(flat.fr.pk.cu)),names(flat.fr.cm.cu))
 vars.use
 
 flat.merged.cu <- bind_rows(list(Sk_Fraser= select(flat.fr.sk.cu,any_of(vars.use)),
@@ -65,7 +65,7 @@ flat.merged.cu <- bind_rows(list(Sk_Fraser= select(flat.fr.sk.cu,any_of(vars.use
                               .id = "DataSet")
 
 sort(unique(flat.merged.cu$CU_Name))
-sum(flat.merged.cu$CU_Name=="Osoyoos-L",na.rm=TRUE)
+sum(flat.merged.cu$CU_Name=="OsoyoosLake-V1",na.rm=TRUE)
 
 # GP ADDED March 2023: Filter out any records before CU-specific start year
 start.yrs.df <-  read.csv("DATA_LOOKUP_FILES/MOD_MAIN_CU_LOOKUP_FOR_SOS.csv",stringsAsFactors = FALSE) %>%
@@ -82,7 +82,7 @@ head(flat.merged.cu)
 
 
 sort(unique(flat.merged.cu$CU_Name))
-sum(flat.merged.cu$CU_Name=="Osoyoos-L",na.rm=TRUE)
+sum(flat.merged.cu$CU_Name=="OsoyoosLake-V1",na.rm=TRUE)
 
 
 
